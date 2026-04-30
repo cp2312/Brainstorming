d750f93b9c3789855c11308761cd1b1b4e76a356
cp2312 <fernandopuli59@gmail.com>
2026-04-30 14:45:46 -0500

    version mejorada Brainstorming

--- api/chatbot.js
+++ api/chatbot.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch("https://api.anthropic.com/v1/messages", {
+    const response = await fetch("/api/chatbot", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
